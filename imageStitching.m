7c8afae14fa957fa9150ae0e0c2b99f1d8478365
Kurtis Van Gent <kurtis.vangent@coyotes.usd.edu>
2015-11-05 20:54:47 -0600

    Revert "LOOK AT ME COMMIT"
    This reverts commit 6c701a1e257e954dde102ac1e5a93305b11ea8a6.

--- imageStitching.m
+++ imageStitching.m
@@ -1,8 +1,7 @@
-function [ output_args ] = imageStitching( input_args )
+`function [ output_args ] = imageStitching( input_args )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 
-HOLY SHIT THIS SHOULDNT BE HERE
 end
 
