12daee88343ed66b329a19f0cd6bfc361bea7e1f
Carly Wilson <Carly.Jo.Wilson2011@gmail.com>
2015-11-05 20:44:01 -0600

    I added a line of text
    Wrote in a new line of text for a demonstration

--- imageStitching.m
+++ imageStitching.m
@@ -1,4 +1,4 @@
-function [ output_args ] = imageStitching( input_args )
+`function [ output_args ] = imageStitching( input_args )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
