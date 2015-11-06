6c701a1e257e954dde102ac1e5a93305b11ea8a6
Kurtis Van Gent <kurtis.vangent@coyotes.usd.edu>
2015-11-05 20:52:39 -0600

    LOOK AT ME COMMIT
    stuff

--- imageStitching.m
+++ imageStitching.m
@@ -1,7 +1,8 @@
-`function [ output_args ] = imageStitching( input_args )
+function [ output_args ] = imageStitching( input_args )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 
+HOLY SHIT THIS SHOULDNT BE HERE
 end
 
