f54958d1eafb264839439cab9a1752ce53372730
Kurtis Van Gent <kurtis.vangent@coyotes.usd.edu>
2015-11-18 21:26:09 -0600

    Changed variable names and added comments

--- imageStitching.m
+++ imageStitching.m
@@ -1,47 +1,48 @@
-function [ output_args ] = imageStitching(imgLoc)
-    %Load Images
-    imgDir = 'C:\Users\Kurtis\Documents\School\CSC 492\Proj\Panorama-ImageStitching\images\Building';
-    bDir =  fullfile(toolboxdir('vision'), 'visiondata', 'building');
-    imgSet = imageSet(imgDir);
+function [ output_img ] = imageStitching(imgSet)
+%Pre: imgSet is a image set of pictures to be sitched together
+%created with imageSet('IMAGE_DIR')
     montage(imgSet.ImageLocation);
     
-    %Read the first image and Intialize features
-    I = read(imgSet, 1);
-    grayImg = rgb2gray(I);
-    points = detectSURFFeatures(grayImg);
-    [features, points] = extractFeatures(grayImg, points);
-    % Initialize all the transforms to the identity matrix using a project
-    % transform
+    %Read the first image
+    img = read(imgSet, 1);
+    %convert to gray image
+    grayImg = rgb2gray(img);
+    %find intial features
+    pts = detectSURFFeatures(grayImg);
+    [features, pts] = extractFeatures(grayImg, pts);
+    % Initialize all the transforms to the identity matrix using a project transform
     tforms(imgSet.Count) = projective2d(eye(3));
     
     %Go through the rest of the images
     for n = 2:imgSet.Count
-        pointsPrevious = points;
-        featuresPrevious = features;
+        %Save the last points for comparison
+        ptsPrevious = pts;
+        featuresPrev = features;
         
-        %Read nth imagee and extract features
-        I = read(imgSet, n);
-        grayImg = rgb2gray(I);
-        points = detectSURFFeatures(grayImg);
-        [features, points] = extractFeatures(grayImg, points);
+        %Read next image and extract features of it
+        img = read(imgSet, n);
+        grayImg = rgb2gray(img);
+        pts = detectSURFFeatures(grayImg);
+        [features, pts] = extractFeatures(grayImg, pts);
         
-        %Match points between n and n-1
-        indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
-        matchedPoints = points(indexPairs(:,1), :);
-        matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);
+        %Find matching pts between this image and the last one
+        indexPairs = matchFeatures(features, featuresPrev, 'Unique', true);
+        matchedPoints = pts(indexPairs(:,1), :);
+        matchedPointsPrev = ptsPrevious(indexPairs(:,2), :);
         
-        %Estimate the tranformation between n and n-1
+        %Estimate the tranformation between this image and the last one
+        %based on the the matching features
         tforms(n) = estimateGeometricTransform(matchedPoints, matchedPointsPrev,...
-            'projective', 'Confidence', 99.9, 'MaxNumTrials', 2000);
+            'projective', 'Confidence', 99.9, 'MaxNumTrials', 5000);
         tforms(n).T =  tforms(n-1).T * tforms(n).T;
     end
     
     %Get the limits of each transform
-    imageSize = size(I);
+    imageSize = size(img);
     for i = 1:numel(tforms)
         [xlim(i,:),ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
     end
-    %Use limits to calculate center image
+    %Use boundarys of limits to calculate which image should be in the center
     avgXLim = mean(xlim, 2);
     [~, idx] = sort(avgXLim);
     centerIdx = floor((numel(tforms)+1)/2);
@@ -51,38 +52,39 @@ function [ output_args ] = imageStitching(imgLoc)
     for i = 1:numel(tforms)
         tforms(i).T = Tinv.T * tforms(i).T;
     end
-    %Calculate the size of panorama
+    %Calculate the size of panorama based on the boundaries on the images
     for i = 1:numel(tforms)
         [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]);
     end
-    %find the min and max output limits
+    %find the min and max output limits for x and y
     xMin = min([1; xlim(:)]);
     xMax = max([imageSize(2); xlim(:)]);
     
     yMin = min([1; ylim(:)]);
     yMax = max([imageSize(1); ylim(:)]);
-    %Create the panoramic
+    %Create the panoramic based on the min and max boundaries
     width = round(xMax - xMin);
     height = round(yMax - yMin);
-    panorama = zeros([height width 3], 'like', I);
+    panorama = zeros([height width 3], 'like', img);
     
-    %Move the images into the panorama
+    %Create blender to put images together
     blender = vision.AlphaBlender('Operation', 'Binary mask', ...
         'MaskSource', 'Input port');
-    
+    %Calculate the reverences between images
     xLimits = [xMin xMax];
     yLimits = [yMin yMax];
-    panoramaView = imref2d([height width], xLimits, yLimits);
-    
+    panoramaRef = imref2d([height width], xLimits, yLimits);
+    %For every image in the set, put into panorama
     for i = 1:imgSet.Count
-        I = read(imgSet, i);
-        % transform images into the pandorama
-        warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+        img = read(imgSet, i);
+        % transform images into the panorama
+        warpedImage = imwarp(img, tforms(i), 'OutputView', panoramaRef);
         % Overlay the warpedImage onto the panorama
         panorama = step(blender, panorama, warpedImage, warpedImage(:,:,1));
     end
     
+    output_img = panorama;
     figure
-    imshow(panorama)
+    imshow(output_img)
 end
 
